b55d4eae12715c6ecae27ab31d2fdf59cbe1171e
sypai <7suyashbajpai@gmail.com>
2019-12-13 16:03:39 +0530

    Updates

---
+++ precache-manifest.85f2760c1ccc41874a7e244aa7101311.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eadf5a2d6f5bff0603067df22dccdd2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b871b7ff3715473c486a",
+    "url": "/monsters-rolodex/static/css/main.85816529.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "b871b7ff3715473c486a",
+    "url": "/monsters-rolodex/static/js/main.2bf10540.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
