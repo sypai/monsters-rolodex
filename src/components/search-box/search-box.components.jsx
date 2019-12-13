c3508075fe801c64859bf71de2269d66ca2dea92
sypai <7suyashbajpai@gmail.com>
2019-12-13 15:50:34 +0530

    v1.0

---
+++ src/components/search-box/search-box.components.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './search-box.styles.css'
+
+export const SearchBox = ({placeholder, handleChange}) => 
+(
+    <input 
+        className='search-box'
+        type='search' 
+        placeholder={placeholder} 
+        onChange={handleChange}
+    />
+);
